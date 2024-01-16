62c6ba7cc1f856ae367b828e6e78e65886e00130
Juan Pimentel <juandbpimentel@alu.ufc.br>
2024-01-15 23:13:21 -0300

    corrigindo problema de diretório base

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export'
-}
+  output: "export",
+  basePath: "/frontend",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
